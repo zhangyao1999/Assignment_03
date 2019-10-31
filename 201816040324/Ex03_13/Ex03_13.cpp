7b07506e6b289d63c021519a14964003291935bf
zhangyao1999 <56502471+zhangyao1999@users.noreply.github.com>
2019-10-31 21:05:28 +0800

    Create  Ex03_13.cpp

---
+++ 201816040324/Ex03_13/Ex03_13.cpp
@@ -0,0 +1,16 @@
+// Lab 3: EmployeeTest.cpp
+// Create and manipulate two Employee objects.
+#include <iostream>
+using namespace std;
+
+#include "Invoice.h" // include definition of class Employee
+
+// function main begins program execution
+int main()
+{
+    Invoice invoice1("computer", "6666", 6500, 12) ;
+    invoice1.diaplay () ;
+
+} // end main
+
+
